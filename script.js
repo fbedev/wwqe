d0cd6ca53d14f7c24872ac355509d89e5ac2412c
fbe_dev <97958311+fbedev@users.noreply.github.com>
2023-11-25 15:22:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.botpressWebChat.init({
     "composerPlaceholder": "Chat with bot",
-    "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
+    "botConversationDescription": "by fbedev",
     "botId": "c1805a82-4b30-4fcd-95ee-fa42da7885f4",
     "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
     "messagingUrl": "https://messaging.botpress.cloud",
